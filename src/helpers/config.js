051bcb9a06d69c63e414499e7803b419bf4e3827
Iuri Chiba <dom_ichiba@uolinc.com>
2019-11-05 21:48:26 -0300

    Constants & Configurations added, Sandbox as a configuration

---
+++ src/helpers/config.js
@@ -0,0 +1,8 @@
+
+// Navigation: initial route, sandbox mode, etc.
+navigation = {
+	initialRoute: "FirstScreen",
+	sandboxMode: false	
+}
+
+export default { navigation };
--- src/navigators/MainNavigator.js
+++ src/navigators/MainNavigator.js
@@ -1,27 +1,41 @@
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-import FirstScreen from './../screens/FirstScreen'
-import Sandbox from './../screens/Sandbox'
+// Constants
+import Configurations from "../helpers/config";
 
+// Screens
+import FirstScreen from "./../screens/FirstScreen";
+import Sandbox from "./../screens/Sandbox";
+
+// Helper Functions
+function getInitialRoute() {
+  const {
+    navigation: { sandboxMode, initialRoute }
+  } = Configurations;
+  return sandboxMode ? "Sandbox" : initialRoute;
+}
+
+// Constructor
 const MainNavigator = createStackNavigator(
   {
-  	FirstScreen: {
-  		screen: FirstScreen,
-  		navigationOptions: {
-  			header: null
-  		}
-  	},
+    FirstScreen: {
+      screen: FirstScreen,
+      navigationOptions: {
+        header: null
+      }
+    },
     Sandbox: {
       screen: Sandbox,
       navigationOptions: {
-      	title: "Sandbox"
+        title: "Sandbox"
       }
     }
   },
   {
-  	initialRouteName: 'Sandbox'
+    initialRouteName: getInitialRoute()
   }
-)
+);
 
-export default createAppContainer(MainNavigator)
+// Export
+export default createAppContainer(MainNavigator);
