856400068197ed2aacdccebeb3b33ff35844f2f5
Iuri Chiba <dom_ichiba@uolinc.com>
2019-11-05 21:07:16 -0300

    Sandbox screen added

--- src/navigators/MainNavigator.js
+++ src/navigators/MainNavigator.js
@@ -2,15 +2,25 @@ import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 
 import FirstScreen from './../screens/FirstScreen'
+import Sandbox from './../screens/Sandbox'
 
 const MainNavigator = createStackNavigator(
   {
-    FirstScreen: {
-      screen: FirstScreen,
+  	FirstScreen: {
+  		screen: FirstScreen,
+  		navigationOptions: {
+  			header: null
+  		}
+  	},
+    Sandbox: {
+      screen: Sandbox,
       navigationOptions: {
-        header: null
+      	title: "Sandbox"
       }
     }
+  },
+  {
+  	initialRouteName: 'Sandbox'
   }
 )
 
