c1164ac9368b494964fb2c0475cd3ca7afd6ac2e
Mateus Costa <cin_msilveira@uolinc.com>
2019-11-19 11:51:33 -0300

    fix

--- src/reducers/itemsReducer.js
+++ src/reducers/itemsReducer.js
@@ -5,25 +5,21 @@ const nanoid = require('nanoid/non-secure'); // https://github.com/ai/nanoid
 import itemsMock from './items.mock'
 
 var newItems;
-let count = 0;
-export default function itemsReducer(items = [], action) {
+export default function itemsReducer(state = itemsMock, action) {
   switch (action.type) {
     case ADD_ITEM:
       let currentDate = new Date();
       currentDate.setHours(0, 0, 0, 0);
-      let currentItems = _.clone(items.filter(item => item.month.getMonth() === currentDate.getMonth())) || [];
+      let currentItems = _.clone(state.filter(item => item.month.getMonth() === currentDate.getMonth())) || [];
       if (currentItems.length > 0) {
         if (currentItems[0].items.filter(item => item.day.getDay() === currentDate.getDay()).length === 0)
           currentItems[0].items.push({ day: currentDate, ...action.item });
         else {
           let obj = currentItems[0].items.find(item => item.day.getDay() === currentDate.getDay());
-          console.log('==============obj')
-          console.log(obj)
-          console.log(++count)
-          console.log('=============\n')
           if (obj) {
-            currentItems[0].items[currentItems[0].items.lastIndexOf()] = { ...action.item };
-            alert(currentItems[0].items[currentItems[0].items.lastIndexOf()].productionTime)
+            obj = { ...action.item, day: currentDate };
+            currentItems[0].items.splice(currentItems[0].items.lastIndexOf(), 1);
+            currentItems[0].items.push(obj);
           }
         }
       } else
@@ -31,13 +27,10 @@ export default function itemsReducer(items = [], action) {
           month: new Date(currentDate.getFullYear(), currentDate.getMonth()),
           items: [{ day: currentDate, ...action.item }]
         });
-      newItems = currentItems;
-      console.log('================================')
-      console.log(_.difference(items, currentItems))
-      console.log('================================')
+      newItems = _.merge(state, currentItems);
       return newItems
     case UPDATE_ITEM:
-      newItems = [...items];
+      newItems = [...state];
       newItems = newItems.map(item => {
         if (item.id === action.item.id) {
           const updatedItem = {
@@ -57,6 +50,6 @@ export default function itemsReducer(items = [], action) {
       });
       return newItems
     default:
-      return items
+      return state
   }
 }
