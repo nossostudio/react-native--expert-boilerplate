d126ed7ff98699427384710b01963671379c42e9
Alexandre Lage <bk_alage@uoledtech.corp>
2019-11-25 17:15:38 -0300

    Added haptics feedback

--- src/components/Touchable.tsx
+++ src/components/Touchable.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TouchableNativeFeedback, TouchableOpacity, Platform } from 'react-native';
-
+import * as Haptics from 'expo-haptics';
 
 export default function Touchable(props) {
-    return Platform.OS === "android" ? <TouchableNativeFeedback {...props} /> : <TouchableOpacity {...props} />
+    return Platform.OS === "android" ? <TouchableNativeFeedback {...props} onPress={function onPress() { props.onPress(); Haptics.selectionAsync(); }} /> : <TouchableOpacity {...props} onPress={function onPress() { props.onPress(); Haptics.selectionAsync(); }} />
 }
